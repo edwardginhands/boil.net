e39cadb9bb8f8dc75dbceb31537d4526a66f90e1
Mark Cuckson <mark.cuckson@bt.com>
2015-10-21 15:36:42 +0100

    with chart

--- Boiler/app/products/boilerCtrl.js
+++ Boiler/app/products/boilerCtrl.js
@@ -11,9 +11,15 @@
         //vm.boiler = { isElementOn: true};
         boilerResource.query(function (data) { vm.boiler = data; });
 
-        $scope.$on('statusService-received-data-event', function (event, args) {
-            $scope.status1 = { element: "true" };
+        $scope.gaugeInit = false;
+        $scope.chartOptions = {
+            width: 100, height: 100,
+            redFrom: 90, redTo: 100,
+            yellowFrom: 75, yellowTo: 90,
+            minorTicks: 5
+        };
 
+        $scope.$on('statusService-received-data-event', function (event, args) {
             if (args.element != vm.boiler.isElementOn || args.pump != vm.boiler.isPumpOn) {
 
                 vm.boiler.isElementOn = args.element;
@@ -21,7 +27,21 @@
                 $scope.$apply();
             }
 
-           // $scope.vm.boiler = $
+
+
+            if (!$scope.gaugeInit) {
+                $scope.chartData = google.visualization.arrayToDataTable([
+                    ['Label', 'Value'],
+                    ['Temp', 0]
+                ]);
+
+                var div = document.getElementById('chartDiv');
+                $scope.chart = new google.visualization.Gauge(div);
+                $scope.gaugeInit = true;
+            }
+            $scope.chartData.setValue(0, 1, Math.round(args.temp));
+            $scope.chart.draw($scope.chartData, $scope.chartOptions);
+
         });
     }
 
